9e18ff3431655331016495c4fd7c654ff7b8eaf7
Andrey <andrey.smol5@gmail.com>
2025-01-22 02:23:09 +0300

    added bd methods

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,11 +2,63 @@ package main
 
 import (
 	"RESTAPIporject/internal/config"
-	"fmt"
+	"RESTAPIporject/internal/lib/logger/sl"
+	"RESTAPIporject/internal/storage/sqlite"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Printf("%+v\n", cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting server")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	id, err := storage.SaveURL("https://google.com", "google")
+	if err != nil {
+		log.Error("failed to save url", sl.Err(err))
+		os.Exit(1)
+	}
+
+	log.Info("saved url", slog.Int64("id", id))
+
+	id, err = storage.SaveURL("https://google.com", "google")
+	if err != nil {
+		log.Error("failed to save url", sl.Err(err))
+		os.Exit(1)
+	}
+
+	log.Info("saved url", slog.Int64("id", id))
+
+	_ = storage
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
+
+	return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,26 +1,29 @@
 package config
 
 import (
-	"github.com/ilyakaznacheev/cleanenv"
 	"log"
 	"os"
 	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Env         string `yaml:"env" env:"ENV" env-default:"local"`
-	StoragePath string `yaml:"storage-path" env-required:"true"`
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
 	HTTPServer  `yaml:"http_server"`
 }
 
 type HTTPServer struct {
-	Address      string        `yaml:"address" env-default:"localhost:8080"`
-	Timeout      time.Duration `yaml:"timeout" env-default:"4s"`
-	IdleTimeoutt time.Duration `yaml:"idle-timeout" env-default:"60s"`
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	User        string        `yaml:"user" env-required:"true"`
+	Password    string        `yaml:"password" env-required:"true" env:"HTTP_SERVER_PASSWORD"`
 }
 
 func MustLoad() *Config {
-	configPath := os.Getenv("CONFIG_PATH")
+	configPath := "E://Other//GoProjects//RESTAPIporject//config//local.yaml"
 	if configPath == "" {
 		log.Fatal("CONFIG_PATH is not set")
 	}
@@ -33,9 +36,8 @@ func MustLoad() *Config {
 	var cfg Config
 
 	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
-		log.Fatalf("error reading config file: %s", err)
+		log.Fatalf("cannot read config: %s", err)
 	}
 
 	return &cfg
-
 }
